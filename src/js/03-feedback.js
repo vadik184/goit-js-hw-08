e2602e7b793f1dae34014341a89c7db7adb7fe91
vadik184 <133372327+vadik184@users.noreply.github.com>
2023-09-21 01:00:01 +0300

    xczcx

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+let formData = {};
+const getNewData = throttle(function (event) {
+  formData[event.target.name] = event.target.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}, 500);
+form.addEventListener('input', getNewData);
+
+try {
+  if (localStorage.getItem('feedback-form-state')) {
+    formData = JSON.parse(localStorage.getItem('feedback-form-state'));
+    for (let key in formData) {
+      form.elements[key].value = formData[key];
+    }
+  }
+} catch (error) {
+  console.log(error);
+}
+function makeSubmit(event) {
+  event.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+  console.log(formData);
+}
+form.addEventListener('submit', makeSubmit);
